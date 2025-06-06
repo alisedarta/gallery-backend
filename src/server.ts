a568017167f85bd43bdfc81a319626a00f76da1a
alisedarta <a2000s@inbox.lv>
2025-06-06 13:40:39 +0200

    feat: make this backend work with all frontend features abs-352

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from "express";
 import router from "./api/galleryItem.controller";
 import { connectDB } from "./infrastructure/db";
 
+const cors = require("cors");
 const app = express();
 connectDB();
 const PORT = 3000;
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is healthy" });
 });
